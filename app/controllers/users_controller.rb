a258916458a3ff32fb3fc1d05ee533b9ce1572b6
Tufahel <tufahel96@gmail.com>
2022-08-30 00:27:32 +0600

    linters fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-    load_and_authorize_resource
-    def index; end
+  load_and_authorize_resource
+  def index; end
 
-    def show; end
+  def show; end
 end
