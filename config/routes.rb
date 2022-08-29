77538cd89f9e9d7e1b9794260bc4bbb95a9180cc
Tufahel <tufahel96@gmail.com>
2022-08-30 00:22:19 +0600

    schema and model change a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'user/sessions',
+    registrations: 'user/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  resources :foods, only: [:index]
+  root "foods#index"
+  resources :foods, only: [:index, :create, :show, :destory, :new]
   resources :recipes, only: [:index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_132107) do
     t.string "measurement_unit"
     t.float "price"
     t.integer "quantity"
-    t.integer "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -52,4 +53,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_132107) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
 end
