13b0528f82ab5e9087a30d43cdd8791ed0bd6ce7
Tufahel <tufahel96@gmail.com>
2022-09-01 16:52:01 +0600

    linters fixed

--- app/models/food.rb
+++ app/models/food.rb
@@ -10,5 +10,4 @@ class Food < ApplicationRecord
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   validates :measurement_unit, presence: true, uniqueness: { case_sensitive: false }
-  
 end
